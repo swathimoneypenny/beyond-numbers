0116aced0b1f5689eb537a09e076316e6a44edc1
Swathi Suresh <swathi_suresh@moneypennyllc.com>
2026-07-22 17:43:50 +0530

    Land users on /workshops after sign in or sign up

--- src/lib/authRoutes.js
+++ src/lib/authRoutes.js
@@ -1,3 +1,3 @@
 /* Where users land after a successful sign in / sign up.
    Change this one constant to point auth at a different landing page. */
-export const AFTER_AUTH_REDIRECT = '/checklists'
+export const AFTER_AUTH_REDIRECT = '/workshops'
